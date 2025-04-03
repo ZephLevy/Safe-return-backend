96c7c8cd1c71cc49bccdef2e8705c3b7da9e427e
Zeph <write2zeph@gmail.com>
2025-04-03 23:15:38 +0200

    Now, only POST methods are permitted
    Also, fixed typo...

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+
+		time := r.Form.Get("time")
+
+		fmt.Println("Got time: " + time)
+		w.Write([]byte("Received time: " + time))
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
