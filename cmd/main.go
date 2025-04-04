535b8a90f4ee42e9d64e059f5de41568db10783b
Zeph <write2zeph@gmail.com>
2025-04-04 14:08:12 +0200

    More descriptive naming

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func main() {
 		}
 	}()
 
-	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/setTime", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
