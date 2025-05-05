4053262a83335b60c4004de1845befaf63adf371
Zeph <write2zeph@gmail.com>
2025-05-05 12:55:21 +0200

    Add a basic signin endpoint

--- internal/endpoints/endpointmanager.go
+++ internal/endpoints/endpointmanager.go
@@ -7,6 +7,7 @@ import (
 )
 
 func OpenEndpoints() {
+	startLoginListen()
 	startCheckListen()
 	fmt.Println("Started listening on port: " + listenPort)
 	log.Fatal(http.ListenAndServe(":"+listenPort, nil))
---
+++ internal/endpoints/signin.go
@@ -0,0 +1,40 @@
+package endpoints
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"net/http"
+)
+
+func startLoginListen() {
+	http.HandleFunc("/signIn", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+
+		username := r.Form.Get("username")
+		password := r.Form.Get("password")
+
+		if username == "" || password == "" {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+
+		// HACK: Don't use SHA256
+		h := sha256.New()
+		h.Write([]byte(password))
+		passwordHash := h.Sum(nil)
+
+		fmt.Println(username + password + "\n" + string(passwordHash))
+
+		// TODO: Validate email, make sure user doesn't already exist, etc...
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Login Successful"))
+	})
+}
