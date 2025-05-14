a46901753222bdb79eacbe71b0f18332ed97269d
Zeph <write2zeph@gmail.com>
2025-05-14 13:54:47 +0200

    Align db column naming with proper naming conventions

--- db/init.sql
+++ db/init.sql
@@ -4,7 +4,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
-    passwordHash TEXT NOT NULL
+    password_hash TEXT NOT NULL
 );
 
 CREATE TABLE emergency_contacts (
